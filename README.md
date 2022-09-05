9de8b53e6c14179a7bdba5e0f878fe0a57a16a97
Aaron Dickman <aaronldickman@gmail.com>
2022-09-04 18:26:50 -0600

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# nobloatfandom
+# No Bloat Fandom
 Chrome extension to remove the crap from fandom.com wikis.
---
+++ extension/fandom-script.js
@@ -0,0 +1,41 @@
+const ELEMENTS_TO_REMOVE = [
+    ".global-navigation",
+    ".global-footer",
+    ".page__right-rail",            // "others like you also viewed..." content sidebar
+    ".render-wiki-recommendations", // "others like you also viewed..." content footer
+    "#mixed-content-footer",        // recs for other unrelated wikis
+    "#WikiaBar"                     // links to fandom socials in bottom right
+].join(", ");
+
+// https://stackoverflow.com/questions/32533580/deleting-dom-elements-before-the-page-is-displayed-to-the-screen-in-a-chrome-ex/32537455#32537455
+const observer = new MutationObserver(onMutation);
+onMutation([{ addedNodes: [document.documentElement] }]);
+observe();
+
+
+function onMutation(mutations) {
+    let stopped;
+    for (const { addedNodes } of mutations) {
+        for (const node of addedNodes) {
+            if (node.tagName) {
+                if (node.matches(ELEMENTS_TO_REMOVE)) {
+                    stopped = true;
+                    observer.disconnect();
+                    node.remove();
+                } else if (node.firstElementChild && node.querySelector(ELEMENTS_TO_REMOVE)) {
+                    stopped = true;
+                    observer.disconnect();
+                    for (const el of node.querySelectorAll(ELEMENTS_TO_REMOVE)) el.remove();
+                }
+            }
+        }
+    }
+    if (stopped) observe();
+}
+
+function observe() {
+    observer.observe(document, {
+        subtree: true,
+        childList: true,
+    });
+}
