32ca2b31f9e97cd5023f9c6f9746e5273fe2405c
Aaron Dickman <aaronldickman@gmail.com>
2023-07-03 12:46:48 -0600

    refactor code to use es6 classes

--- extension/fandom-script.js
+++ extension/fandom-script.js
@@ -1,7 +1,7 @@
 const useMinifiedNavBar = 'use-minified-nav-bar'
 const showNavBar = 'show-nav-bar';
 const showWikiaBar = 'show-wikiabar';
-const configValues = [useMinifiedNavBar, showNavBar, showWikiaBar]
+const configKeys = [useMinifiedNavBar, showNavBar, showWikiaBar]
 
 const ELEMENTS_TO_REMOVE = [
     ".global-footer",
@@ -14,112 +14,117 @@ const ELEMENTS_TO_REMOVE = [
     ".top-ads-container",
     ".bottom-ads-container"
 ].join(", ");
-
 const GLOBAL_NAVIGATION_BAR = ".global-navigation";
 const GLOBAL_NAVIGATION_BAR_TOP_CONTENT = ".global-navigation__top";
 const WIKIA_BAR = "#WikiaBar";
 const STICKY_HEADER = ".fandom-sticky-header"
-let currentlyMinifyingNav;
-let currentlyHidingNav;
+const DOM_CHANGE_REQUIRED_ELS = [ELEMENTS_TO_REMOVE, GLOBAL_NAVIGATION_BAR, WIKIA_BAR, STICKY_HEADER].join(", ");
 
-chrome.storage.sync.get(configValues).then(loadedValues => {
-    let observationHalted;
-    currentlyMinifyingNav = loadedValues[useMinifiedNavBar];
-    currentlyHidingNav = !loadedValues[showNavBar];
+function minifyNavBar(minify) {
+    const navbar = document.querySelector(GLOBAL_NAVIGATION_BAR);
+    const navbarTopContent = document.querySelector(GLOBAL_NAVIGATION_BAR_TOP_CONTENT);
+    if (minify) {
+        navbar.classList.add("global-navigation--minified");
+        navbarTopContent.classList.add("global-navigation__top--hidden");
+    } else {
+        navbar.classList.remove("global-navigation--minified");
+        navbarTopContent.classList.remove("global-navigation__top--hidden");
+    }
+}
 
-    initializeConfigListeners();
-    const observer = new MutationObserver(onMutation);
-    onMutation([{ addedNodes: [document.documentElement] }]);
-    observe();
+function hideNavBar(hide) {
+    const navbar = document.querySelector(GLOBAL_NAVIGATION_BAR);
+    hide ? navbar.classList.add("global-navigation--hidden")
+        : navbar.classList.remove("global-navigation--hidden");
+}
 
-    function onMutation(mutations) {
-        function handleConfigCases(node) {
-            if (node.matches(GLOBAL_NAVIGATION_BAR)) {
-                haltObservation()
-                hideNavBar(!loadedValues[showNavBar])
-                minifyNavBar(loadedValues[useMinifiedNavBar]);
-            } else if (node.matches(WIKIA_BAR)) {
-                haltObservation()
-                hideWikiaBar(!loadedValues[showWikiaBar])
-            }
-        }
-        const mutatedNodes = mutations.flatMap(m => Array.from(m.addedNodes))
+function updateStickyHeader(navbarIsModified) {
+    const stickyHeader = document.querySelector(".fandom-sticky-header")
+    navbarIsModified ? stickyHeader.classList.add("fandom-sticky-header--navbar-modified")
+        : stickyHeader.classList.remove("fandom-sticky-header--navbar-modified")
+}
 
-        mutatedNodes.forEach(node => {
-            if (!node.tagName) return;
+function hideWikiaBar(hide) {
+    const wikiaBar = document.querySelector("#WikiaBar");
+    hide ? wikiaBar.classList.add("wikia-bar--hidden")
+        : wikiaBar.classList.remove("wikia-bar--hidden");
+}
 
-            handleConfigCases(node);
-            if (node.matches(ELEMENTS_TO_REMOVE)) {
-                haltObservation()
-                node.remove();
-            }
-            else if (node.matches(STICKY_HEADER)) {
-                haltObservation()
-                updateStickyHeader(currentlyHidingNav || currentlyMinifyingNav);
-            }
-            else if (node.firstElementChild && node.querySelector(ELEMENTS_TO_REMOVE)) {
-                haltObservation()
-                node.querySelectorAll(ELEMENTS_TO_REMOVE).forEach(el => el.remove())
-            }
-        });
-        if (observationHalted) observe();
+class FandomObserverOrchestrator {
+    observationHalted = false;
+    config;
+    observer;
+
+    getConfig() {
+        return chrome.storage.sync.get(configKeys)
     }
 
-    function haltObservation() {
-        observationHalted = true;
-        observer.disconnect();
+    haltObservation = () => {
+        this.observationHalted = true;
+        this.observer.disconnect();
     }
 
-    function observe() {
-        observer.observe(document, {
+    observe = () => {
+        this.observer.observe(document, {
             subtree: true,
             childList: true,
         });
     }
 
-    function initializeConfigListeners() {
+    initializeConfigListeners = () => {
         chrome.storage.sync.onChanged.addListener(changes => {
-            if (changes[useMinifiedNavBar] !== undefined) {
-                currentlyMinifyingNav = changes[useMinifiedNavBar].newValue
-                minifyNavBar(changes[useMinifiedNavBar].newValue)
-                updateStickyHeader(currentlyHidingNav || currentlyMinifyingNav)
+            configKeys.forEach(key => {
+                if (key in changes) this.config[key] = changes[key].newValue;
+            });
+            if (useMinifiedNavBar in changes) {
+                console.log('useminifiednavbar')
+                minifyNavBar(this.config[useMinifiedNavBar])
+                updateStickyHeader(!this.config[showNavBar] || this.config[useMinifiedNavBar])
             }
-            if (changes[showNavBar] !== undefined) {
-                currentlyHidingNav = !changes[showNavBar].newValue;
-                hideNavBar(!changes[showNavBar].newValue)
-                updateStickyHeader(currentlyHidingNav || currentlyMinifyingNav)
+            if (showNavBar in changes) {
+                hideNavBar(!this.config[showNavBar])
+                updateStickyHeader(!this.config[showNavBar] || this.config[useMinifiedNavBar])
             }
-            if (changes[showWikiaBar] !== undefined) hideWikiaBar(!changes[showWikiaBar].newValue)
+            if (showWikiaBar in changes) {
+                hideWikiaBar(!this.config[showWikiaBar])
+            }
         })
     }
 
-    function minifyNavBar(minify) {
-        const navbar = document.querySelector(GLOBAL_NAVIGATION_BAR);
-        const navbarTopContent = document.querySelector(GLOBAL_NAVIGATION_BAR_TOP_CONTENT);
-        if (minify) {
-            navbar.classList.add("global-navigation--minified");
-            navbarTopContent.classList.add("global-navigation__top--hidden");
-        } else {
-            navbar.classList.remove("global-navigation--minified");
-            navbarTopContent.classList.remove("global-navigation__top--hidden");
-        }
+    onMutation = (mutations) => {
+        const mutatedNodes = mutations.flatMap(m => Array.from(m.addedNodes))
+
+        mutatedNodes.forEach(node => {
+            if (!node.tagName) return;
+            if (!node.matches(DOM_CHANGE_REQUIRED_ELS)) return;
+
+            this.haltObservation();
+            if (node.matches(GLOBAL_NAVIGATION_BAR)) {
+                hideNavBar(!this.config[showNavBar])
+                minifyNavBar(this.config[useMinifiedNavBar]);
+            } else if (node.matches(WIKIA_BAR)) {
+                hideWikiaBar(!this.config[showWikiaBar])
+            } else if (node.matches(STICKY_HEADER)) {
+                updateStickyHeader( !this.config[showNavBar] || this.config[useMinifiedNavBar]);
+            } else if (node.matches(ELEMENTS_TO_REMOVE)) {
+                node.remove();
+            } else if (node.firstElementChild && node.querySelector(ELEMENTS_TO_REMOVE)) {
+                node.querySelectorAll(ELEMENTS_TO_REMOVE).forEach(el => el.remove())
+            }
+        });
+        if (this.observationHalted) this.observe();
     }
 
-    function hideNavBar(hide) {
-        const navbar = document.querySelector(GLOBAL_NAVIGATION_BAR);
-        hide ? navbar.classList.add("global-navigation--hidden")
-            : navbar.classList.remove("global-navigation--hidden");
+    start() {
+        this.getConfig().then(config => {
+            this.config = config;
+            this.initializeConfigListeners();
+            this.observer = new MutationObserver(this.onMutation);
+            this.onMutation([{ addedNodes: [document.documentElement] }]);
+            this.observe();
+        })
     }
+}
 
-    function updateStickyHeader(navbarIsModified) {
-        const stickyHeader = document.querySelector(".fandom-sticky-header")
-        navbarIsModified ? stickyHeader.classList.add("fandom-sticky-header--navbar-modified")
-            : stickyHeader.classList.remove("fandom-sticky-header--navbar-modified")
-    }
-
-    function hideWikiaBar(hide) {
-        const wikiaBar = document.querySelector("#WikiaBar");
-        hide ? wikiaBar.classList.add("wikia-bar--hidden")
-            : wikiaBar.classList.remove("wikia-bar--hidden");
-    }
-});
+const orchestrator = new FandomObserverOrchestrator();
+orchestrator.start();
